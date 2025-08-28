b1699216a653c64041c450d137118adc7e55d32c
mak3551 <173456597+mak3551@users.noreply.github.com>
2025-08-28 19:32:05 +0900

    Add rating_history property in Player Class

--- glicko2/player.py
+++ glicko2/player.py
@@ -1,18 +1,21 @@
 from glicko2 import glicko2
+from datetime import date
 
 
 class Player:
     """
-    A player has id and rating.
+    A player has id, rating, and rating history.
     id must be unique.
     """
 
     id: int
     rating: glicko2.Rating
+    rating_history: list[tuple[date, glicko2.Rating]]
 
     def __init__(self, id: int, rating: glicko2.Rating):
         self.id = id
         self.rating = rating
+        self.rating_history = []
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, Player):
