8e2eba32f75bcfc4eb93f9a885b6fadff3dbff60
mak3551 <173456597+mak3551@users.noreply.github.com>
2025-08-28 16:39:27 +0900

    correct spell

--- glicko2/glicko2.py
+++ glicko2/glicko2.py
@@ -27,8 +27,8 @@ R_INITIAL: float = 1500
 RD_INITIAL: float = 350
 SIGMA_INITIAL: float = 0.06
 
-# In the Mark Glickman's paper,is is "τ" (in Step 1). It constrains the change in volatility over time.
-# He says Reasonable choices are between 0.3 and 1.2,
+# In the Mark Glickman's paper,it is "τ" (in Step 1). It constrains the change in volatility over time.
+# He says reasonable choices are between 0.3 and 1.2,
 # though the system should be tested to decide which value results in greatest predictive accuracy.
 # Smaller values of τ prevent the volatility measures from changing by large amounts.
 TAU: float = 1.0
