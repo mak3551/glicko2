eaeef6d35601c135ed5b4978dd409a4bd733a00b
mak3551 <173456597+mak3551@users.noreply.github.com>
2025-08-30 07:21:30 +0900

    ADD Glicko2_np in __init__.py

--- glicko2/__init__.py
+++ glicko2/__init__.py
@@ -1,5 +1,15 @@
 from .glicko2 import Glicko2, Rating, WIN, DRAW, LOSS
 from .player import Player
 from .rate_period import rate_period
+from .glicko2_np.glicko2_np import Glicko2_np
 
-__all__ = ["Glicko2", "Rating", "WIN", "DRAW", "LOSS", "Player", "rate_period"]
+__all__ = [
+    "Glicko2",
+    "Rating",
+    "WIN",
+    "DRAW",
+    "LOSS",
+    "Player",
+    "rate_period",
+    "Glicko2_np",
+]
