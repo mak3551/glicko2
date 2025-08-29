a045ff8dcad6a02f66f5997dd41ba79c364dd571
mak3551 <173456597+mak3551@users.noreply.github.com>
2025-08-29 09:35:08 +0900

    implemented rate_period()

--- glicko2/rate_period.py
+++ glicko2/rate_period.py
@@ -1,9 +1,13 @@
 from glicko2.player import Player
-from glicko2.glicko2 import Rating
+from glicko2.glicko2 import Rating, Glicko2
+from datetime import date
 
 
 def rate_period(
-    matches: list[tuple[Player, Player, float]], players_list: list[Player]
+    matches: list[tuple[Player, Player, float]],
+    players_list: list[Player],
+    system: Glicko2 | None = None,
+    match_date: date | None = None,
 ) -> list[Player]:
     """
     calculates rates of each player in a rating period.
@@ -14,10 +18,21 @@ def rate_period(
             For example, if player1 wins against player2, (player1, player2, 1)
             If one player's matches against same player, like (player1, player1, 1) exist, error occurred.
         players_list : list of players.
+        system : optional. Glicko2 object. If it doesn't set, internaly generated (tau is default).
+        match_date : optional. If it is set, rating_history of each player is updated.
     """
-
-    # not implemented
-    return []
+    new_players_list: list[Player] = []
+    if system is None:
+        system = Glicko2()
+    for player in players_list:
+        series: list[tuple[float, Rating]] = _get_series_of_player(matches, player)
+        new_rating: Rating = system.rate(player.rating, series)
+        new_rating_history: list[tuple[date, Rating]] = player.rating_history.copy()
+        if match_date is not None:
+            new_rating_history.append((match_date, new_rating))
+        new_player: Player = Player(player.id, new_rating, new_rating_history)
+        new_players_list.append(new_player)
+    return new_players_list
 
 
 def _get_series_of_player(
