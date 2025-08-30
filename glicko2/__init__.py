947c89f48b9c74ee3dd8e1359e35468b3a4adbe8
mak3551 <173456597+mak3551@users.noreply.github.com>
2025-08-30 11:35:28 +0900

    CHANGE numpy from optional to dependency

--- glicko2/__init__.py
+++ glicko2/__init__.py
@@ -1,11 +1,5 @@
-import contextlib
-
 from .glicko2 import DRAW, LOSS, WIN, Glicko2, Rating
-
-# numpy is optional, so if importerror occurred it would be ignored
-with contextlib.suppress(ImportError):
-    from .glicko2_np.glicko2_np import Glicko2Np
-
+from .glicko2_np.glicko2_np import Glicko2Np
 from .player import Player
 from .rate_period import rate_period
 
