7a9c022a54ed34e1f14f937d2644c7f9f315bdb1
mak3551 <173456597+mak3551@users.noreply.github.com>
2025-09-08 22:02:54 +0900

    CHANGE default from glicko2Np to glicko2 (by benchmark result), and add comments,error

--- glicko2/game_rate_calculate.py
+++ glicko2/game_rate_calculate.py
@@ -3,7 +3,7 @@ from datetime import date, timedelta
 from .game_player.game_player import GamePlayerList
 from .game_player.player import Player
 from .rate_period import rate_period
-from .rating_system.glicko2_np.glicko2_np import Glicko2Np
+from .rating_system.glicko2.glicko2 import Glicko2
 from .rating_system.rating_system import RatingSystem
 
 
@@ -16,18 +16,23 @@ def _extract_player_list_from_gamelist(
     argument is a list like this:
         [(date, name_1, name_2, result), (date, name_1, name_2, result), ...]
     date could be str or date. If str, it must be ISO style like "2000-04-01"
-    name_1 and name_2 must not be same.
+    If date is invalid str, it raises ValueError.
+    all date must be same type. If not, it raises TypeError.
+    name_1 and name_2 must not be same. If so, it raises ValueError.
     result is float. if name_1 wins, result is 1.0. If name_1 loses, result is 0.0.
     If draw, result is 0.5.
 
     It returns a sorted gamelist and a list of players extracted from it.
     """
+    # sorted() raises TypeError if first element of gamelist is not same
+    # i.e. [('2020-1-1','a','b',1.0),(date(2020, 3, 4),'c','d',0.0),...] raises TypeError
     sorted_gamelist: list[tuple[str | date, str, str, float]] = sorted(gamelist, key=lambda x: x[0])
     playerlist: GamePlayerList = GamePlayerList()
     for game in sorted_gamelist:
+        # date.isofromformat() raises ValueError if game[0] is invalid format string.
         match_date: date = game[0] if isinstance(game[0], date) else date.fromisoformat(game[0])
         if game[1] == game[2]:
-            raise RuntimeError
+            raise ValueError
         playerlist.add_game_player(game[1], match_date)
         playerlist.add_game_player(game[2], match_date)
 
@@ -83,10 +88,12 @@ def calculate_rating_in_rate_period(
     """
     list_gameplayer = player_list.list_game_player
     player_list_before_rate_period: list[Player] = []
+    # player_list_before_rate_period is list of players whose first matche is before rating_date
     for gameplayer in list_gameplayer:
         if gameplayer.first_mach_date < rating_date:
             player_list_before_rate_period.append(gameplayer.player)
     matchlist_rate_period: list[tuple[Player, Player, float]] = []
+    # convert matches from tuple[int,int,float] to list[Player,Player,float]
     for g in sorted_gamelist_in_rate_period:
         g_player1_id: int = g[0]
         g_player2_id: int = g[1]
@@ -112,7 +119,7 @@ def game_rate_calculate(
     If draw, result is 0.5.
     """
     if rating_system is None:
-        rating_system = Glicko2Np()
+        rating_system = Glicko2()
     sorted_gamelist, player_list = _extract_player_list_from_gamelist(gamelist)
     divided_gamelist = _divide_sorted_gamelist(sorted_gamelist, player_list, per_days)
     for game_rate_period in divided_gamelist:
